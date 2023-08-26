e80d6374f1c1a27b2e90b695d10b974c376b7d50
Pranjal <Parasg8799gupta@gmail.com>
2023-08-26 16:54:55 +0530

    first

--- factorial.cpp
+++ factorial.cpp
@@ -6,7 +6,11 @@ int main() {
 
     cout << "Enter a positive integer: ";
     cin >> n;
-
+    
+    if(n<0){ cout <<"wrong number";
+    return 1;
+	}
+	
     for(int i = 1; i <= n; ++i) {
         factorial *= i;
     }
